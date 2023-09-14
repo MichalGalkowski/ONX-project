5628afd6fd306b4138ba1d64b7b659f153762ba9
Michal <mggalkowski@gmail.com>
2023-09-14 16:53:46 +0200

    first commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>Index page</h2>
+        <Link href="/show">Second page</Link>
+    </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h2>Second page</h2>
+        <Link href="/">Index page</Link>
+    </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
